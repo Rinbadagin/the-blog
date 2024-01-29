ab9cae222310190a700c10f3ab5e4a10acfb0f9a
Rinbadagin <60410893+Rinbadagin@users.noreply.github.com>
2024-01-29 13:35:48 +1300

    Fixed li margins and added raw mode to article content

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -3,13 +3,14 @@ module ArticlesHelper
     html_content = ""
     first_pass = ""
     temp_content = ""
+    mode = nil
     article.body.each_line do |line|
       if line.include?("![") && line.include?(']') && line.split("![")[1].split("]")[0]
         #get content between first ![ and ]
         up = Upload.find_by(title: line.split("![")[1].split("]")[0])
         if up
           url = url_for(Upload.find_by(title: line.split("![")[1].split("]")[0]).content)
-          temp_content = "#{line.split("![")[0]}#{url || "Couldn't find link" }#{line.split("]")[1..].join}"
+          temp_content += "#{line.split("![")[0]}#{url || "Couldn't find link" }#{line.split("]")[1..].join}"
         else 
           temp_content += "#{line.split("![")[0]}![Couldn't find link]#{line.split("]")[1..].join}"
         end
@@ -18,25 +19,37 @@ module ArticlesHelper
       end
     end
     temp_content.each_line do |line|
-      if line.starts_with? "######"
-        html_content += "<h6>#{line.delete_prefix "######"}</h6>"
-      elsif line.starts_with? "#####"
-        html_content += "<h5>#{line.delete_prefix "#####"}</h5>"
-      elsif line.starts_with? "####"
-        html_content += "<h4>#{line.delete_prefix "####"}</h4>"
-      elsif line.starts_with? "###"
-        html_content += "<h3>#{line.delete_prefix "###"}</h3>"
-      elsif line.starts_with? "##"
-        html_content += "<h2>#{line.delete_prefix "##"}</h2>"
-      elsif line.starts_with? "#"
-        html_content += "<h1>#{line.delete_prefix "#"}</h1>"
-      elsif line.starts_with? "*"
-        html_content += "<ul><li>#{line.delete_prefix "*"}</li></ul>"
-      elsif line.starts_with? "_"
-        html_content += "<p>#{line.delete_prefix "_"}</p><div class='border'></div>"
-      elsif line.equal? ""
-        html_content += "<br>"
-      else html_content += "<p>#{line}</p>"
+      if line.starts_with? "```"
+        line = line.delete_prefix "```"
+        if mode == :raw_html
+          mode = nil
+        else
+          mode = :raw_html
+        end
+      end
+      if mode == :raw_html
+        html_content += line
+      else
+        if line.starts_with? "######"
+          html_content += "<h6>#{line.delete_prefix "######"}</h6>"
+        elsif line.starts_with? "#####"
+          html_content += "<h5>#{line.delete_prefix "#####"}</h5>"
+        elsif line.starts_with? "####"
+          html_content += "<h4>#{line.delete_prefix "####"}</h4>"
+        elsif line.starts_with? "###"
+          html_content += "<h3>#{line.delete_prefix "###"}</h3>"
+        elsif line.starts_with? "##"
+          html_content += "<h2>#{line.delete_prefix "##"}</h2>"
+        elsif line.starts_with? "#"
+          html_content += "<h1>#{line.delete_prefix "#"}</h1>"
+        elsif line.starts_with? "*"
+          html_content += "<ul><li>#{line.delete_prefix "*"}</li></ul>"
+        elsif line.starts_with? "_"
+          html_content += "<p>#{line.delete_prefix "_"}</p><div class='border'></div>"
+        elsif line.equal? ""
+          html_content += "<br>"
+        else html_content += "<p>#{line}</p>"
+        end
       end
     end
     return html_content
