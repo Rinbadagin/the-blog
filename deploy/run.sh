aa5ba7017b506c5ceee18c6d140cdd87b9e5ee25
Rinbadagin <60410893+Rinbadagin@users.noreply.github.com>
2025-01-04 18:42:18 +1300

    Added deploy flow for Eliza's blog

---
+++ deploy/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+# Deploy changes and run webhook for discord notification
+bundle install;
+bin/rails assets:precompile;
+bin/rails db:migrate;
+bin/rails restart;
+
+curl -X POST https://discord.com/api/webhooks/1269189137626435585/buHg27T3hZNlExY9sGcdf_sve6CA9mTAJjWyBMEFdODJUYlKnMkla_OHvCeXq6BPD3Tb \
+    -d "content=Updated Eliza's blog to $(git log --oneline | head -n 1) via action"
