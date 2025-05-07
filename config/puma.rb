bed38ff9b6eb85a65c46b7ddea93d261752adc1e
Finn Robinson <finnrobinson5@gmail.com>
2025-05-07 12:06:17 +1200

    Set port to 4000 for prod

--- config/puma.rb
+++ config/puma.rb
@@ -23,7 +23,7 @@ end
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 7000 } if ENV.fetch("RAILS_ENV", "development") != "development" 
+port ENV.fetch("PORT") { 4000 } if ENV.fetch("RAILS_ENV", "development") != "development" 
 port ENV.fetch("PORT") { 4000 } if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `environment` that Puma will run in.
