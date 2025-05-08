4d2a2fb538a9af5159739d6f251f1fe206fac12e
Finn Robinson <finnrobinson5@gmail.com>
2025-05-08 18:07:27 +1200

    Iterated site

--- public/ring/transfemring.js
+++ public/ring/transfemring.js
@@ -27,7 +27,8 @@
   }
 
   if (transfemring.index === -1) {
-    document.currentScript.outerHTML = `<div>
+    document.currentScript.outerHTML = `
+    <div class="transfem-ring" style="display: flex; align-items: center; gap: 3.278%; max-width: 244px; image-rendering: pixelated">
       <a href="${transfemring.root}" target="_top"><img src="${urlWithWidget("banner.png")}"></a>
       <div style="font-size: 0.75em; text-align: center">(this site isn't part of the webring... yet!)</div>
     </div>`;
