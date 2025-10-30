73f4c9ce72f0d1486a46c36993074f542593c982
Rinbadagin <klara78robinson@gmail.com>
2025-10-30 19:36:59 +1300

    Amend article form to be 100% width & disable deploy webhook

--- deploy/run.sh
+++ deploy/run.sh
@@ -5,5 +5,6 @@ bin/rails assets:precompile;
 bin/rails db:migrate;
 bin/rails restart;
 
-curl -X POST https://discord.com/api/webhooks/1269189137626435585/buHg27T3hZNlExY9sGcdf_sve6CA9mTAJjWyBMEFdODJUYlKnMkla_OHvCeXq6BPD3Tb \
-    -d "content=Updated Klara's blog (klara.nz) to $(git log --oneline | head -n 1) via action"
+# Bring back another time? :(
+# curl -X POST https://discord.com/api/webhooks/1269189137626435585/buHg27T3hZNlExY9sGcdf_sve6CA9mTAJjWyBMEFdODJUYlKnMkla_OHvCeXq6BPD3Tb \
+#    -d "content=Updated Klara's blog (klara.nz) to $(git log --oneline | head -n 1) via action"
